a32c39022bf3ac9c8c33679306be42d200697132
Jeremy Cook <jeremy.cook@cloudacademy.com>
2024-04-08 15:42:38 +1200

    restructured for lab

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,1 @@
-terraform {
-  required_version = ">= 1.5.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-west-2"
-}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,1 @@
-output "bucket1_name" {
-  value = aws_s3_bucket.bucket1.bucket
-}
+
